306a5d7d36d25605904558de114ed52b2014e862
patelankitk <patelankitk@yahoo.com>
2016-04-25 10:54:12 +0100

    CHECK

---
+++ DayOneDS1/DayOneDS1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DayOneDS1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int guessNumber = 46;
+            int userGuess;
+            do
+            { 
+            Console.WriteLine("Please Enter a Number.");
+            userGuess = int.Parse(Console.ReadLine());
+                 
+                if (userGuess > guessNumber )
+                {
+                    Console.WriteLine("Please guess a lower number.\n");
+                }
+                else if (userGuess < guessNumber)
+                {
+                    Console.WriteLine("Please guess a higher number.\n");
+                }
+            } while (userGuess != guessNumber);
+
+            Console.WriteLine("You Guessed it, the correct guess is {0}.\n", userGuess);
+
+        }
+    }
+}
