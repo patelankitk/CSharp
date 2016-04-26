b6cdc12e2ee16e0c2f87e2d12644f6e1fec9ffa8
patelankitk <patelankitk@yahoo.com>
2016-04-26 21:02:19 +0100

    Add

--- CarSales/CarSales/Program.cs
+++ CarSales/CarSales/Program.cs
@@ -11,31 +11,32 @@ namespace CarSales
     {
         static void Main(string[] args)
         {
-            Car car1 = new Car { make = "audi", model = "A4", milage = 15000, salePrize = 20000 };
-            Car car2 = new Car { make = "BMW", model = "X5", milage = 20000, salePrize = 24000 };
-            Car car3 = new Car { make = "Range Rover", model = "Autobiography", milage = 15000, salePrize = 35000 };
+            //Car car1 = new Car { make = "audi", model = "A4", milage = 15000, salePrice = 20000 };
+            //Car.totalNumberOfCars++;
+            //Car car2 = new Car { make = "BMW", model = "X5", milage = 20000, salePrice = 24000 };
+            //Car.totalNumberOfCars++;
+            //Car car3 = new Car { make = "Range Rover", model = "Autobiography", milage = 15000, salePrice = 35000 };
+            //Car.totalNumberOfCars++;
 
-            Console.WriteLine("Make = {0}, Model = {1}, Milage = {2}, Sale Prize = {3}, Total Number of Cars = {4}", car1.make, car1.model, car1.milage, car1.salePrize,Car.totalNumberOfCars);
-            Console.WriteLine("Make = {0}, Model = {1}, Milage = {2}, Sale Prize = {3}", car2.make, car2.model, car1.milage, car2.salePrize);
-            Console.WriteLine("Make = {0}, Model = {1}, Milage = {2}, Sale Prize = {3}", car3.make, car3.model, car1.milage, car3.salePrize);
+            //Console.WriteLine("Total Number of car in stock is {0}.",Car.totalNumberOfCars);
+            //Console.WriteLine("Make = {0}, Model = {1}, Milage = {2}, Sale Prize = £{3}", car1.make, car1.model, car1.milage, car1.salePrice);
+            //Console.WriteLine("Make = {0}, Model = {1}, Milage = {2}, Sale Prize = £{3}", car2.make, car2.model, car1.milage, car2.salePrice);
+            //Console.WriteLine("Make = {0}, Model = {1}, Milage = {2}, Sale Prize = £{3}", car3.make, car3.model, car1.milage, car3.salePrice);
+            Car car4 = new Car();
+            car4.AddCar("Tesla", "S", 35000, 100);
+            Car car5 = new Car();
+            car5.AddCar("Bentley", "Continental", 180000, 10000);
+            Car car6 = new Car();
+            car6.AddCar("Infinity", "Q30", 30000, 8000);
+            car4.ListCar();
+            car5.ListCar();
+            car6.ListCar();
+            Console.WriteLine("Total Number of car in stock is {0}.", Car.totalNumberOfCars);
+            car6.CarSold(29500, true);
+            Console.WriteLine("Total Number of car in stock is {0}.", Car.totalNumberOfCars);
+
 
 
         }
-
-        
-           
     }
-    
-
-    public class Car
-    {
-        public string make { get; set; }
-        public string model { get; set; }
-        public int salePrize { get; set; }
-        public int milage { get; set; }
-        public static int totalNumberOfCars = 3;
-    }
-
-
-
 }
