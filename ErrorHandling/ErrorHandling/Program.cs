cc7af47e021991022f2ffcc1d094f2251bdbca0a
patelankitk <patelankitk@yahoo.com>
2016-04-26 15:00:38 +0100

    NEW

---
+++ ErrorHandling/ErrorHandling/Program.cs
@@ -0,0 +1,48 @@
+﻿//Error Handling Program //
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ErrorHandling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            // asking user to input an array //
+            Console.WriteLine("Please enter a number to be inserted in an array.");
+            
+            //storing the userinput into userNumber  //
+            int userNumber = int.Parse(Console.ReadLine());
+
+            // creating an array //
+            int[] Numbers = new int[5];
+           
+
+            try
+            {
+                for (int i = 0; i < userNumber; i++)
+                {
+                    Numbers[i] = 2 * (i + 2);
+                    
+                }
+
+                foreach (int number  in Numbers)
+                {
+                    Console.WriteLine(number);
+                }
+            }
+            catch(IndexOutOfRangeException ioor)
+            {
+                Console.WriteLine(ioor.Message);
+                Console.ReadLine();
+            }
+            
+
+        }
+    }
+}
---
+++ ErrorHandlingFile/ErrorHandlingFile/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ErrorHandlingFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader sr = null;
+
+            try
+            {
+                sr = File.OpenText(@"C:\Users\student\mydat.txt");
+                Console.WriteLine(sr.ReadToEnd());
+            }
+            catch (FileNotFoundException fnfe)
+            {
+                Console.WriteLine(fnfe.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
