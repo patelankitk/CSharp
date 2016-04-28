58ae656bfe102b88b07e2e3e7a1e770812bfb771
patelankitk <patelankitk@yahoo.com>
2016-04-28 11:14:30 +0100

    New

---
+++ Restaurant/Restaurant/Accounts.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurant
+{
+    public class Accounts
+    {
+
+        
+        public static decimal TotalBill(Meal meal, decimal balance)
+
+        {
+            balance = balance + meal.price;
+            balance = balance - meal.cost;
+
+            return balance;
+
+        }
+    
+    }
+}
