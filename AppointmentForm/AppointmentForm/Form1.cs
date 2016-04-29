bd0d2179b4cca767fff35e9e6c9a24af0a3f5a28
patelankitk <patelankitk@yahoo.com>
2016-04-29 12:59:44 +0100

    ADD

---
+++ AppointmentForm/AppointmentForm/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AppointmentForm
+{
+    public partial class Form1 : Form
+    {
+        string name = "";
+        string doctorName = "";
+        
+        DateTime date1;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBoxName_TextChanged(object sender, EventArgs e)
+        {
+            name = textBoxName.Text;
+        }
+
+        private void textBoxDoctor_TextChanged(object sender, EventArgs e)
+        {
+            doctorName = textBoxDoctor.Text;
+        }
+
+        private void dateTimePickerAD_ValueChanged(object sender, EventArgs e)
+        {
+            date1 = dateTimePickerAD.Value;
+        }
+
+        private void labelMessage_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void ButtonSubmit_Click(object sender, EventArgs e)
+        {
+            labelMessage.Text =name+" your appointment with Doctor "+doctorName+" on "+ date1.ToShortDateString()+ " has been booked.";
+        }
+    }
+}
---
+++ WindowsFormDemo1/WindowsFormDemo1/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormDemo1
+{
+    public partial class Form1 : Form
+    {
+        string name = "";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBoxName_TextChanged(object sender, EventArgs e)
+        {
+            name = textBoxName.Text;
+        }
+
+        private void buttonMessage_Click(object sender, EventArgs e)
+        {
+            DateTime today = DateTime.Now.Date;
+            TimeSpan ageDays = today - dateTimePicker1.Value;
+            int years = (int)(ageDays.TotalDays / 365);
+            int day = dateTimePicker1.Value.Day;
+            string month = dateTimePicker1.Value.ToString("MMMM");
+
+
+            LabelMessage.Text = "Hello {0} " + name + ", you will be "+(1+years)+ " year old on "+day+" "+month+".";
+            
+        }
+
+        private void buttonClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
