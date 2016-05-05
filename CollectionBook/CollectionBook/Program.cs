a9b8e1b79d2c34039ceb9b8c250b4415bd6996f7
patelankitk <patelankitk@yahoo.com>
2016-05-05 22:59:33 +0100

    Add

--- CollectionBook/CollectionBook/Program.cs
+++ CollectionBook/CollectionBook/Program.cs
@@ -10,32 +10,18 @@ namespace CollectionBook
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("\n\n\n*********************************************Book Collection.***************************************************");
+            Console.WriteLine("\n\n\n\t\t\tBook Collection.");
+
 
             List<books> BookList = new List<books>();
 
-            books new book;
+            books book1 = new books("C#","A",12345,12.35);
+            books book2 = new books("Java", "B", 123456, 25);
+            books book3 = new books("Java Script", "C", 1234567, 30);
 
-            //books book1 = new books();
-            //book1.title1 = "C#";
-            //book1.author1 = "A";
-            //book1.ISBN1 = 12345;
-            //book1.price1 = 12.35;
-            //BookList.Add(book1);
-
-            //books book2 = new books();
-            //book2.title = "Java";
-            //book2.author = "B";
-            //book2.ISBN = 123456;
-            //book2.price = 25;
-            //BookList.Add(book2);
-
-            //books book3 = new books();
-            //book3.title = "Java Script";  
-            //book3.author = "C";
-            //book3.ISBN = 1234567;
-            //book3.price = 30;
-            //BookList.Add(book3);
+            BookList.Add(book1);         
+            BookList.Add(book2);
+            BookList.Add(book3);
 
             string UserChoice;
             do
@@ -46,16 +32,13 @@ namespace CollectionBook
                 switch (UserChoice)
                 {
                     case "L":
-
-                        foreach (books b in BookList)
-                        {
-                            Console.WriteLine("\nBook List is as follows :\nTitle : {0} \nAuthor :{1} \nISBN :{2} \nPrice :{3}", b.title, b.author, b.ISBN, b.price);
-                        }
+                        books.ListBook(BookList);                        
                         break;
 
                     case "A":
+
                         Console.WriteLine("Please enter a the Name of new book:");
-                        string BookName =Console.ReadLine();
+                        string BookName = Console.ReadLine();
 
                         Console.WriteLine("Please enter a the Author of new book:");
                         string BookAuthor = Console.ReadLine();
@@ -64,32 +47,17 @@ namespace CollectionBook
                         int BookISBN = int.Parse(Console.ReadLine());
 
                         Console.WriteLine("Please enter a the Price of new book:");
-                        double BookPrice =double.Parse(Console.ReadLine());
+                        double BookPrice = double.Parse(Console.ReadLine());
 
-                        
-                            BookList.AddNewBooks(BookName,BookAuthor,BookISBN,BookPrice);
-                            BookList.All(books.AddNewBooks(BookName, BookAuthor, BookISBN, BookPrice));
-                        
+                        books.AddBook(BookName, BookAuthor, BookISBN, BookPrice, BookList);
 
 
-
+                        //    BookList.AddNewBooks(BookName,BookAuthor,BookISBN,BookPrice);
+                        //    BookList.All(books.AddNewBooks(BookName, BookAuthor, BookISBN, BookPrice));
                         break;
 
                     case "R":
-
-                        Console.WriteLine("Please Enter the Book title you wish to remove.");
-                        string bookRemove =Console.ReadLine();
-
-                        //for (int i = 0; i < BookList.Count; i++)
-                        //{
-                        //    if (BookList[i].title.ToLower() == bookRemove.ToLower() )
-                        //    {
-                        //        BookList.RemoveAt(i);
-                        //        Console.WriteLine("\nYour book with title {0} has been removed.", bookRemove);
-                        //    }                       
-                        //}
-
-                        
+                        books.RemoveBooks(BookList);
                         break;
 
                     default:
--- CollectionBook/CollectionBook/books.cs
+++ CollectionBook/CollectionBook/books.cs
@@ -8,10 +8,10 @@ namespace CollectionBook
 {
     class books
     {
-        public static string title;
-        public static string author;
-        public static int ISBN;
-        public static double price;
+        public string title;
+        public string author;
+        public int ISBN;
+        public double price;
 
         public books()
         {
@@ -19,24 +19,49 @@ namespace CollectionBook
         }
         
 
-        public void AddBooks(string title1, string author1, int ISBN1, double price1)
+        public books(string title, string author, int ISBN, double price)
         {
-            title = title1;
-            author = author1;
-            ISBN = ISBN1;
-            price = price1;
+            this.title = title;
+            this.author = author;
+            this.ISBN = ISBN;
+            this.price = price;
         }
 
-        public static void AddNewBooks (string title1, string author1, int ISBN1, double price1)
+        
+
+        public static void ListBook(List<books> b1)
         {
-            title = title1;
-            author = author1;
-            ISBN = ISBN1;
-            price = price1;
+            foreach (books b in b1)
+            {
+                Console.WriteLine("\nBook List is as follows :\nTitle : {0} \nAuthor :{1} \nISBN :{2} \nPrice :{3}", b.title, b.author, b.ISBN, b.price);
+            }
+        } 
 
-           // List<books> BookList = new List<books>();
-            
+        public static void AddBook(string title, string author, int ISBN, double price,List<books> b1)
+        {
+            books b = new books();
+            b.title = title;
+            b.author = author;
+            b.ISBN = ISBN;
+            b.price = price;
+            b1.Add(b);
 
+        }
+
+       
+        public static void RemoveBooks(List<books> b1)
+        {
+            Console.WriteLine("Please Enter the Book title you wish to remove.");
+            string bookRemove = Console.ReadLine();
+
+            for (int i = 0; i < b1.Count; i++)
+            {
+                if (b1[i].title.ToLower() == bookRemove.ToLower())
+                {
+                    b1.RemoveAt(i);
+                    Console.WriteLine("\nYour book with title {0} has been removed.", bookRemove);
+                }
+            }
         }
 
 
